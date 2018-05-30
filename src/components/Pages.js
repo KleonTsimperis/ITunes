5ef206f7fe4a69848086144ba185d46935d18bd1
kleon <tsimbiris@hotmail.com>
2018-05-30 13:53:23 +0200

    Pages page display logic update

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -10,12 +10,12 @@ const Pages = props =>
         :
         null
       }
-      { 40 / props.numberOfAlbums < 1 || 40 / props.numberOfAlbums === 1 ?
+      { 40 / props.numberOfAlbums < 2  || 40 / props.numberOfAlbums === 1 ?
         <li className="list-inline-item h2 mx-3" onClick={()=> props.page2()}><a href="#top"> 2 </a> </li>
         :
         null
       }
-      { 60 / props.numberOfAlbums < 1 || 60 / props.numberOfAlbums === 1 ?
+      {  60 / props.numberOfAlbums < 2 && 60 / props.numberOfAlbums < 1.5  || 60 / props.numberOfAlbums === 1 ?
         <li className="list-inline-item h2 mx-3" onClick={()=> props.page3()}><a href="#top"> 3 </a> </li>
         :
         null
